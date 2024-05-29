be33b6f5a3133c50c7f06b2531007502c99f7675
Lungisani Ngcobo <mapholoba066@gmail.com>
2024-05-29 17:03:24 +0200

    add computer choice script

---
+++ script.js
@@ -0,0 +1,11 @@
+function getComputerChoice() {
+  let randomInt = Math.floor(Math.random() * 3);
+  let computerChoice = "Rock";
+  if (randomInt === 1) {
+    computerChoice = "Paper";
+  } else if (randomInt === 2) {
+    computerChoice = "Scissors";
+  } 
+  return computerChoice;
+} 
+
