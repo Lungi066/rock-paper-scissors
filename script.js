ae07c330327ee962fa19f1ced228ad1a128778d7
Lungisani Ngcobo <mapholoba066@gmail.com>
2024-05-29 17:53:38 +0200

    combine playGame components

--- script.js
+++ script.js
@@ -28,23 +28,47 @@ function getHumanChoice() {
   return humanChoice;
 }
 
-/* Score */
-let humanScore = 0;
-let computerScore = 0;
 
-/* Play Round */
-function playRound(humanChoice, computerChoice) {
-  if (
-    (humanChoice === "Rock" && computerChoice === "Scissors") || 
-    (humanChoice === "Paper" && computerChoice === "Rock") ||
-    (humanChoice === "Scissors" && computerChoice === "Paper")
-  ) {
-    humanScore++;
-    alert(`You win! ${humanChoice} beats ${computerChoice}`);
-  } else if (humanChoice === computerChoice) {
-    alert("It's a tie!");
+/* Play Game */
+function playGame() {
+
+  /* Score */
+  let humanScore = 0;
+  let computerScore = 0;
+
+  /* Loop Game */
+  for (let round = 0; round < 5; round++) {
+    const humanSelection = getHumanChoice();
+    const computerSelection = getComputerChoice();
+
+    playRound(humanSelection, computerSelection);
+  }
+  
+  /* Play Round */
+  function playRound(humanChoice, computerChoice) {
+    if (
+      (humanChoice === "Rock" && computerChoice === "Scissors") || 
+      (humanChoice === "Paper" && computerChoice === "Rock") ||
+      (humanChoice === "Scissors" && computerChoice === "Paper")
+    ) {
+      humanScore++;
+      alert(`You win! ${humanChoice} beats ${computerChoice}`);
+    } else if (humanChoice === computerChoice) {
+      alert("It's a tie!");
+    } else {
+      computerScore++;
+      alert(`You lose! ${computerChoice} beats ${humanChoice}`);
+    }
+  }
+  
+  /* Game Results */
+  if (humanScore > computerScore) {
+    alert(`You win! Your score: ${humanScore}. Computer score: ${computerScore}`);
+  } else if (humanScore === computerScore) {
+    alert(`It's a tie! Your score: ${humanScore}. Computer score: ${computerScore}`);
   } else {
-    computerScore++;
-    alert(`You lose! ${computerChoice} beats ${humanChoice}`);
+    alert(`You lose! Your score: ${humanScore}. Computer score: ${computerScore}`);
   }
 }
+
+playGame();
